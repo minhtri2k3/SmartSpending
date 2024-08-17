eab1d4b95374b12c2dce02740af6e8dcfb27c125
minhtri2k3 <quangminhtri2003@gmail.com>
2024-08-17 23:03:09 +0700

    update state

--- lib/blocs/authentiocation_state.dart
+++ lib/blocs/authentiocation_state.dart
@@ -3,9 +3,22 @@ enum Formstatus { initial, pending, success, error }
 class AuthenticationState {
   final String username;
   final String password;
-  final Formstatus formstatus;
+  final Formstatus status;
   final String errormsg;
-  // googo
-  AuthenticationState(this.username, this.password, this.formstatus, this.errormsg);
-
+  AuthenticationState(
+      {required this.username,
+      required this.password,
+      required this.status,
+      required this.errormsg});
+  AuthenticationState copyWith({
+    String? username,
+    String? password,
+    Formstatus? status,
+    String? errormsg,
+  }) =>
+      AuthenticationState(
+          username: username ?? this.username,
+          password: password ?? this.password,
+          status: status ?? this.status,
+          errormsg: errormsg ?? this.errormsg);
 }
