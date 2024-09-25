88225fec797b277efe93dde115598678c932a238
minhtri2k3 <118947935+minhtri2k3@users.noreply.github.com>
2024-09-26 00:20:58 +0700

    Update sm_home_screen.dart

--- lib/screens/sm_home_screen.dart
+++ lib/screens/sm_home_screen.dart
@@ -1,4 +1,3 @@
-
 import '../core/export.dart';
 
 part 'sm_home_screen.g.dart';
@@ -6,7 +5,7 @@ part 'sm_home_screen.g.dart';
 class SMHomeScreenController = _SMHomeScreenController
     with _$SMHomeScreenController;
 
-abstract class _SMHomeScreenController extends SMBaseController with Store{
+abstract class _SMHomeScreenController extends SMBaseController with Store {
   @observable
   bool loading = true;
 
@@ -14,10 +13,8 @@ abstract class _SMHomeScreenController extends SMBaseController with Store{
   void setLoading(bool loading_) => loading = loading_;
 
   @action
-  Future<void> getData() async{
-    try{
-
-    } on DioException catch (error, stackTrace) {
+  Future<void> getData() async {
+    try {} on DioException catch (error, stackTrace) {
       // onApiException(error, stackTrace);
     } catch (error, stackTrace) {
       logger.e(
@@ -30,15 +27,16 @@ abstract class _SMHomeScreenController extends SMBaseController with Store{
 }
 
 class SMHomeScreen extends SMBaseWidget<SMHomeScreenState> {
-  const SMHomeScreen({super.key, required super.state,});
-
+  const SMHomeScreen({
+    super.key,
+    required super.state,
+  });
 
   static SMHomeScreen create({
     Key? key,
     String? instanceName,
   }) {
-    final SMHomeScreenController controller =
-    SMHomeScreenController();
+    final SMHomeScreenController controller = SMHomeScreenController();
     return SMHomeScreen(
       state: SMHomeScreenState(
         controller: controller,
@@ -46,28 +44,63 @@ class SMHomeScreen extends SMBaseWidget<SMHomeScreenState> {
     );
   }
 }
-class SMHomeScreenState  extends SMBaseWidgetState<SMHomeScreenController>{
-    SMHomeScreenState({ required super.controller,});
 
+class SMHomeScreenState extends SMBaseWidgetState<SMHomeScreenController> {
+  SMHomeScreenState({
+    required super.controller,
+  });
 
   @override
   Widget build(BuildContext context) {
-     return mainBuilder(
-         (context) => Scaffold(
-           backgroundColor: Colors.white,
-           body: SafeArea(child: Container(
-             width: double.infinity,
-             height: double.infinity,
-             color: SMColors.white1,
-             child : SingleChildScrollView(
-               child: Column(
-                 
-               ),
-             )
-           ),
-           ),
-         ),
-       onGetServicesDone: () => controller.getData(),
-     );
-  }}
-
+    return mainBuilder(
+      (context) => Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: SMColors.white1,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0xFFCAC4D0),
+                        width: 8,
+                      ),
+                      borderRadius: BorderRadius.circular(28),
+                      color: Color(0xFFFEF7FF),
+                    ),
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 10.sp,
+                            width: double.infinity,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://i.imgur.com/1tMFzp8.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      onGetServicesDone: () => controller.getData(),
+    );
+  }
+}
